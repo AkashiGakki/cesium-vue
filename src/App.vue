bb35c849608d6a5cca012f1a22a90e17658b28d9
akashi_sai <akashi_sai@163.com>
2020-07-26 21:42:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CesiumViewer></CesiumViewer>
+  </div>
+</template>
+
+<script>
+import CesiumViewer from './components/CesiumViewer'
+
+export default {
+  name: 'App',
+  components: {
+    CesiumViewer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CesiumViewer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script>
+import Cesium from 'cesium/Cesium'
+import 'cesium/Widgets/widgets.css'
+
+export default {
+    name: 'CesiumViewer',
+    mounted () {
+        this.init();
+    },
+    methods: {
+        init () {
+            let viewerOption = {
+                geocoder: false,                // 地理位置搜索控件
+                homeButton: false,              // 首页跳转控件
+                sceneModePicker: false,         // 2D,3D和Columbus View切换控件
+                baseLayerPicker: false,         // 三维地图底图切换控件
+                navigationHelpButton: false,    // 帮助提示控件
+                animation: false,               // 视图动画播放速度控件
+                // creditsDisplay: false,       // 版权商标和数据源
+                timeline: false,                // 时间轴控件
+                fullscreenButton: false,        // 全屏控件
+                infoBox: false,                 // 点击显示窗口控件
+                selectionIndicator: false,      // 实体对象选择框控件
+                scene3DOnly: true               // 仅3D渲染，节省GPU内存
+            }
+            // your token
+            Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MTFkZDY2NC00NjE4LTQyOTMtOTZhMS1lNzU4OWJhY2ZkNzgiLCJpZCI6MzE2NDQsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTU3NTkxODV9.pkaTjYrzbEswbhOy2X4KelZpDepVSgxN7f_GVYl6BiQ';
+
+            let viewer = new Cesium.Viewer('cesiumContainer', viewerOption);
+            console.log('viewer', viewer);
+            console.log('cesium', Cesium);
+
+            // 隐藏Logo
+            viewer.cesiumWidget.creditContainer.style.display="none";
+        }
+    }
+}
+</script>
+
+<style>
+#cesiumContainer {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: none;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+</style>
