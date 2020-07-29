a1c996e952753c44ad6c3a3ef7f37c7e5b9017ad
Akashi <akashi_sai@163.com>
2020-07-29 16:54:45 +0800

    added static file plugins support

--- vue.config.js
+++ vue.config.js
@@ -25,13 +25,33 @@ module.exports = {
             }
         },
         plugins: [
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers'}]),
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets'}]),
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}]),
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'ThirdParty/Workers'), to: 'ThirdParty/Workers'}]),
+            new CopyWebpackPlugin([{
+                from: path.join(cesiumSource, cesiumWorkers),
+                to: 'Workers'
+            }]),
+            new CopyWebpackPlugin([{
+                from: path.join(cesiumSource, 'Assets'),
+                to: 'Assets'
+            }]),
+            new CopyWebpackPlugin([{
+                from: path.join(cesiumSource, 'Widgets'),
+                to: 'Widgets'
+            }]),
+            new CopyWebpackPlugin([{
+                from: path.join(cesiumSource, 'ThirdParty/Workers'),
+                to: 'ThirdParty/Workers'
+            }]),
             new webpack.DefinePlugin({
                 CESIUM_BASE_URL: JSON.stringify('./')
-            })
+            }),
+            new CopyWebpackPlugin([{
+                from: path.join('./static', 'model'),
+                to: 'model3D'
+            }]),
+            new CopyWebpackPlugin([{
+                from: path.join('./static', 'images'),
+                to: 'images'
+            }])
         ]
     }
 }
